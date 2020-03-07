eb1c49f199302d1ff4db5132004f2706658849ee
Nils Eriksson <nilser@gmail.com>
2020-03-07 22:09:50 +0100

    Typescript standard package

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Terraform state in Typescript");
+console.log("-----------------------------");
+console.log("CLI not yet implemented.");
+console.log("The tool was called with the following args:");
+console.log(process.argv);
+console.log("-----------------------------");
