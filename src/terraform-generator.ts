ae5a85b16a8a2548b1f7ebc8456eebbfc1d8dd71
Nils Eriksson <nilser@gmail.com>
2020-03-11 13:01:29 +0100

    Cleanup generator

--- src/terraform-generator.ts
+++ src/terraform-generator.ts
@@ -11,58 +11,55 @@ const writeAssignment = (varName: string | undefined) => {
   return "";
 };
 
-const writeObject = (indent: string, b: any) => {
-  let str = "";
-  str += " {\n";
-  Object.keys(b).forEach(key => {
-    str += writeVar(`${indent}  `, `${key}`, b[key]);
-    str += "\n";
-  });
-  str += `${indent}}`;
-  return str;
-};
-const writeVar = (indent: string = "", varName: string | undefined, b: Node) => {
-  let str: string = "";
+const writeObject = (indent: string, b: any) =>
+  [" {", ...Object.keys(b).map(key => writeVar(`${indent}  `, key, b[key])), `${indent}}`].join("\n");
+
+const writeVar = (indent: string = "", varName: string | undefined, b: Node): string => {
   if (b instanceof CData) {
-    str += `${indent}${varName || ""}`;
-    str += writeAssignment(varName);
-    str += toString(b);
-  } else if (b instanceof JsonEncode) {
-    str += `${indent}${varName || ""}`;
-    str += writeAssignment(varName);
-    str += "<<EOF\n";
-    str += JSON.stringify(b.data) + "\n";
-    str += "EOF\n";
-  } else if (b instanceof Multiple) {
-    b.items.forEach(item => {
-      str += `\n${writeVar(`${indent}`, varName, item)}`;
-    });
-  } else if (b instanceof Value) {
-    str += `${indent}${varName || ""} = `;
-    str += writeObject(indent, b.data);
-  } else if (Array.isArray(b)) {
-    str += `${indent}${varName || ""}`;
-    str += writeAssignment(varName);
-    str += "[";
-    str += b.map(item => `\n${writeVar(`${indent}  `, undefined, item)},`).join("");
-    str += `\n${indent}]`;
-  } else if (isString(b)) {
-    str += `${indent}${varName || ""}`;
-    str += writeAssignment(varName);
-    str += `\"${b}\"`;
-  } else if (isNumber(b) || isBoolean(b)) {
-    str += `${indent}${varName || ""}`;
-    str += writeAssignment(varName);
-    str += `${b}`;
-  } else if (typeof b === "object") {
-    str += `${indent}${varName || ""}`;
-    str += writeObject(indent, b);
-  } else {
-    str += `${indent}${varName || ""}`;
-    str += writeAssignment(varName);
-    str += b;
+    return [`${indent}${varName || ""}`, writeAssignment(varName), toString(b)].join("");
   }
-  return str;
+
+  if (b instanceof JsonEncode) {
+    return [
+      `${indent}${varName || ""}`,
+      writeAssignment(varName),
+      "<<EOF\n",
+      JSON.stringify(b.data) + "\n",
+      "EOF\n",
+    ].join("");
+  }
+
+  if (b instanceof Multiple) {
+    return b.items.map(item => `\n${writeVar(`${indent}`, varName, item)}`).join("");
+  }
+
+  if (b instanceof Value) {
+    return [`${indent}${varName || ""} = `, writeObject(indent, b.data)].join("");
+  }
+
+  if (Array.isArray(b)) {
+    return [
+      `${indent}${varName || ""}`,
+      writeAssignment(varName),
+      "[",
+      b.map(item => `\n${writeVar(`${indent}  `, undefined, item)},`).join(""),
+      `\n${indent}]`,
+    ].join("");
+  }
+
+  if (isString(b)) {
+    return [`${indent}${varName || ""}`, writeAssignment(varName), `\"${b}\"`].join("");
+  }
+
+  if (isNumber(b) || isBoolean(b)) {
+    return [`${indent}${varName || ""}`, writeAssignment(varName), `${b}`].join("");
+  }
+
+  if (typeof b === "object") {
+    return [`${indent}${varName || ""}`, writeObject(indent, b)].join("");
+  }
+
+  return [`${indent}${varName || ""}`, writeAssignment(varName), b].join("");
 };
 
 export const createFileContent = (element: TerraformElement): string => {
